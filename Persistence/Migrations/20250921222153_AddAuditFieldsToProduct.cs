ca6a251cdbdce9ebb48540ca081d1eb2f3d52fbe
Kevin Pereira <pereirak@gmatc.matc.edu>
2025-09-21 17:34:02 -0500

    Complete Activity 4: Product API with full CRUD operations

---
+++ Persistence/Migrations/20250921222153_AddAuditFieldsToProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditFieldsToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastUpdateDate",
+                table: "Products",
+                newName: "LastUpdatedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastUpdatedDate",
+                table: "Products",
+                newName: "LastUpdateDate");
+        }
+    }
+}
