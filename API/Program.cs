4ebd2d86555fc4602bca96d0b07c44045ca54b80
Kevin Pereira <pereirak@gmatc.matc.edu>
2025-10-28 19:38:22 -0500

    Complete Activity 7: React Frontend Setup & Product Display

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,21 @@ using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+       });
+});
+
+ // Add services to the container.
+ builder.Services.AddControllers();
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>();
 
@@ -9,8 +24,12 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+// Use CORS
+app.UseCors("AllowReactApp");
 
-if (app.Environment.IsDevelopment())
+ // Configure the HTTP request pipeline.
+ if (app.Environment.IsDevelopment())
+
 {
     app.MapOpenApi();
 
